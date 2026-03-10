0c5557de0e515c8468d81c84e9944c996ad767b4
tusharthumar <tusharthumar@tes.com>
2026-03-10 13:02:41 +0530

    Add Github Actions Workflow ad shell script

---
+++ scripts/hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "==============================="
+echo "  GitHub Actions Shell Script  "
+echo "==============================="
+echo "Running on: $(uname -a)"
+echo "Current directory: $(pwd)"
+echo "Date: $(date)"
+echo "Script executed successfully!"
